3c6d2dce9272d9d2f8c26fc51dbebb55c2da1677
ardarww <79516457+ardarww@users.noreply.github.com>
2023-10-17 22:33:34 +0300

    dasdasd
    sadsdsadas

---
+++ config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    clientSecret: "U8C2NTatWHZ6uRMug4NZiGyM6xka_zHS",
+    clientID: "1159513813738078219",
+    token: "MTE1OTUxMzgxMzczODA3ODIxOQ.GCf5Ep.WPFxqMaDZOqTOMy-O0KtzhqdH6k9gXmdfzjaV4",
+    mongo: "mongodb+srv://alcoverhesabi:qwerty999@cluster0.1iqvjas.mongodb.net/?retryWrites=true&w=majority",
+    prefix: ".",
+    redirectURI: "http://188.132.198.191:8080",
+    port: 8080,
+    owners: ["781266106014105610"],
+    redirectionIP: ["http://188.132.198.191:8080"],
+    botlink: ["https://discord.com/api/oauth2/authorize?client_id=1159513813738078219&permissions=8&scope=applications.commands+bot"],
+    redirectionBot: ["http://188.132.198.191:8080"],
+    authlink: "https://discord.com/api/oauth2/authorize?client_id=1159513813738078219&redirect_uri=http%3A%2F%2F188.132.198.191%3A8080&response_type=code&scope=identify%20guilds.join",
+
+    webhookURL: "https://discord.com/api/webhooks/1159600053242044476/D9pHi8ROPn4YWQA0LH61qeFcbjeUdCMoMUvsV1O23KIrcwpFjocSPeNuNcBwISehZDZg",
+
+
+   webhookURL2: "https://discord.com/api/webhooks/1159600219701399652/544jjIAF4qCBqmwRiSoou2HVmFrGbsaTePUkHUpDwriHsYx7hSy4e10f0BoVcIrOHggss"
+}
---
+++ index.js
@@ -0,0 +1,3 @@
+const oAuthClient = require('./src/Structures/oAuth');
+const client = new oAuthClient();
+client.start("MTE1OTUxMzgxMzczODA3ODIxOQ.GCf5Ep.WPFxqMaDZOqTOMy-O0KtzhqdH6k9gXmdfzjaV4");
